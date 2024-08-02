46d49f7a98320932499b7a78cf2386fdc9cded62
MUrban <manuel.urban1999@gmail.com>
2024-08-02 10:56:42 +0200

    tm_rework

--- sponet_cv_testing/computation/interpretable_cvs/transition_manifold.py
+++ sponet_cv_testing/computation/interpretable_cvs/transition_manifold.py
@@ -7,12 +7,253 @@ from scipy.sparse.linalg import ArpackNoConvergence, ArpackError
 logger = logging.getLogger("testpipeline.compute.transition_manifold")
 
 
+def compute_transition_manifold(samples: np.ndarray,
+                                bandwidth_transitions: float,
+                                num_coordinates: int,
+                                distance_matrix_triangle_inequality_speedup: bool = False,
+                                optimize_diffusion_map_bandwidth: bool = True,
+                                bandwidth_diffusion_map: float | None = None
+                                ):
+
+    if distance_matrix_triangle_inequality_speedup:
+        distance_matrix, _ = _numba_dist_matrix_gaussian_kernel_triangle_speedup(samples, bandwidth_transitions)
+    else:
+        distance_matrix, _ = _numba_dist_matrix_gaussian_kernel(samples, bandwidth_transitions)
+
+    if optimize_diffusion_map_bandwidth:
+        bandwidth_diffusion_map, dimension_estimate, _, _, _ = optimize_bandwidth_diffusion_map(distance_matrix)
+    assert bandwidth_diffusion_map is not None
+
+    try:
+        eigenvalues, eigenvectors = calc_diffusion_maps(distance_matrix, num_coordinates, bandwidth_diffusion_map)
+    except (ArpackError, ArpackNoConvergence) as e:
+        logger.debug("Arpackerror occurred. Checking if bandwidth is below threshold")
+
+        bandwidth_tolerance: float = np.max(distance_matrix) / np.sqrt(20)
+        if bandwidth_diffusion_map < bandwidth_tolerance:
+            logger.debug(
+                f"#slb Sigma {bandwidth_diffusion_map} smaller than bandwidth tolerance: {bandwidth_tolerance}. "
+                f"Set sigma to {bandwidth_tolerance}")
+            eigenvalues, eigenvectors = calc_diffusion_maps(
+                distance_matrix, num_coordinates, bandwidth_tolerance
+            )
+        else:
+            logger.error("Bandwidth not below threshold.")
+            raise e
+
+    return eigenvectors.real[:, 1:] * eigenvalues.real[np.newaxis, 1:]
+
+
+def optimize_bandwidth_diffusion_map(distance_matrix: np.ndarray):
+
+    epsilons = np.logspace(-6, 2, 101)
+    s = []
+    for epsilon in epsilons:
+        s.append(np.mean(np.exp(-(distance_matrix ** 2) / epsilon)))
+    s = np.array(s)
+
+    derivative = _central_differences(np.log(epsilons), np.log(s))
+    optim_idx = np.argmax(derivative)
+    optim_epsilon = epsilons[optim_idx]
+    optim_bandwidth_diffusion_map = optim_epsilon ** 0.5
+    dimension_estimate = derivative[optim_idx]
+
+    logger.debug(f"Optimal bandwidth: {optim_bandwidth_diffusion_map}")
+
+    return optim_bandwidth_diffusion_map, dimension_estimate, epsilons, s, derivative
+
+
+@njit(parallel=True)
+def _numba_dist_matrix_gaussian_kernel(
+        x_samples: np.ndarray, sigma: float
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Parameters
+    ----------
+    x_samples : np.ndarray
+        Shape = (num_anchor_points, num_samples, dimension).
+    sigma : float
+        Bandwidth.
+
+    Returns
+    -------
+    tuple[np.ndarray, np.ndarray]
+        1) Distance matrix, shape = (num_anchor_points, num_anchor_points).
+        2) kernel matrix diagonal, shape = (num_anchor_points,)
+    """
+    num_anchor, num_samples, dimension = x_samples.shape
+
+    # compute symmetric kernel evaluations
+    kernel_diagonal = np.zeros(num_anchor)
+    for i in range(num_anchor):
+        kernel_diagonal[i] = _numba_gaussian_kernel_eval(
+            x_samples[i], x_samples[i], sigma
+        )
+    # compute asymmetric kernel evaluations and assemble distance matrix
+    distance_matrix = np.zeros((num_anchor, num_anchor))
+    for i in prange(num_anchor):
+        for j in range(i):
+            this_sum = _numba_gaussian_kernel_eval(x_samples[i], x_samples[j], sigma)
+            distance_matrix[i, j] = (
+                    kernel_diagonal[i] + kernel_diagonal[j] - 2 * this_sum
+            )
+    distance_matrix /= num_samples ** 2
+    return distance_matrix + np.transpose(distance_matrix), kernel_diagonal
+
+
+@njit(parallel=False)
+def _numba_dist_matrix_gaussian_kernel_triangle_speedup(
+        x_samples: np.ndarray, sigma: float, triangle_speedup_tolerance=10e-4
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+        Parameters
+        ----------
+        x_samples : np.ndarray
+            Shape = (num_anchor_points, num_samples, dimension).
+        sigma : float
+            Bandwidth.
+        triangle_speedup_tolerance : float
+            Tolerance for relative difference between lower and upper triangle inequality bound for applying the
+            speedup.
+            Defaults to 10e-4.
+
+        Returns
+        -------
+        tuple[np.ndarray, np.ndarray]
+            1) Distance matrix, shape = (num_anchor_points, num_anchor_points).
+            2) kernel matrix diagonal, shape = (num_anchor_points,)
+        """
+    num_anchor, num_samples, dimension = x_samples.shape
+
+    # compute symmetric kernel evaluations
+    kernel_diagonal = np.zeros(num_anchor)
+    for i in range(num_anchor):
+        kernel_diagonal[i] = _numba_gaussian_kernel_eval(
+            x_samples[i], x_samples[i], sigma
+        )
+
+    # compute asymmetric kernel evaluations and assemble distance matrix
+
+    n_speedup = 0
+
+    distance_matrix = np.zeros((num_anchor, num_anchor))
+    for i in range(num_anchor):
+        for j in range(i):
+
+            # Check if triangle inequality speedup can be applied
+            lower_max = -np.inf
+            upper_min = np.inf
+            for k in range(j):
+                lower_triang_bound = np.abs(distance_matrix[i, k] - distance_matrix[j, k])
+                upper_triang_bound = distance_matrix[i, k] + distance_matrix[j, k]
+
+                if lower_triang_bound > lower_max:
+                    lower_max = lower_triang_bound
+                if upper_triang_bound < upper_min:
+                    upper_min = upper_triang_bound
+
+            # If difference is within tolerance
+            if (upper_min - lower_max) / lower_max < triangle_speedup_tolerance:
+                distance_matrix[i, j] = 0.5 * (lower_max + upper_min)
+                n_speedup = n_speedup + 1
+                continue
+
+            this_sum = _numba_gaussian_kernel_eval(x_samples[i], x_samples[j], sigma)
+            distance_matrix[i, j] = (
+                    kernel_diagonal[i] + kernel_diagonal[j] - 2 * this_sum
+            )
+
+    distance_matrix /= num_samples ** 2
+    return distance_matrix + np.transpose(distance_matrix), kernel_diagonal
+
+
+@njit(parallel=False)
+def _numba_gaussian_kernel_eval(x: np.ndarray, y: np.ndarray, sigma: float):
+    """
+    Parameters
+    ----------
+    x : np.ndarray
+        shape = (# x points, dimension)
+    y : np.ndarray
+        shape = (# y points, dimension)
+    sigma : float
+        bandwidth
+
+    Returns
+    -------
+    float
+        sum of kernel matrix
+    """
+    nx = x.shape[0]
+    ny = y.shape[0]
+
+    X = np.sum(x * x, axis=1).reshape((nx, 1)) * np.ones((1, ny))
+    Y = np.sum(y * y, axis=1) * np.ones((nx, 1))
+    out = X + Y - 2 * np.dot(x, y.T)
+    out /= -(sigma ** 2)
+    np.exp(out, out)
+    return np.sum(out)
+
+
+def calc_diffusion_maps(
+        distance_matrix: np.ndarray, num_components: int, sigma: float, alpha: float = 0.5
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Solve diffusion map eigenproblem
+
+    Parameters
+    ----------
+    distance_matrix : np.ndarray
+        Symmetric array of shape (n_features, n_features).
+    num_components : int
+        Number of eigenvalues and eigenvectors to compute.
+    sigma : float
+        Diffusion map kernel bandwidth parameter.
+    alpha : float
+        Diffusion map normalization parameter.
+
+    Returns
+    -------
+    tuple[np.ndarray, np.ndarray]
+        Tuple of (eigenvalues, eigenvectors).
+    """
+
+    num_points = distance_matrix.shape[0]
+    kernel_matrix = np.exp(-(distance_matrix ** 2) / sigma ** 2)
+    row_sum = np.sum(kernel_matrix, axis=0)
+
+    # compensating for testpoint density
+    kernel_matrix = kernel_matrix / np.outer(row_sum ** alpha, row_sum ** alpha)
+
+    # row normalization
+    kernel_matrix = kernel_matrix / np.tile(sum(kernel_matrix, 0), (num_points, 1))
+
+    # weight matrix
+    weight_matrix = np.diag(sum(kernel_matrix, 0))
+
+    # solve the diffusion maps eigenproblem
+    return sla.eigs(kernel_matrix, num_components + 1, weight_matrix)
+
+
+def _central_differences(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """
+    Compute dy/dx via central differences.
+    """
+    out = np.zeros(len(x))
+    for i in range(len(x)):
+        upper_idx = min(i + 1, len(x) - 1)
+        lower_idx = max(i - 1, 0)
+        out[i] = (y[upper_idx] - y[lower_idx]) / (x[upper_idx] - x[lower_idx])
+    return out
+
+
+# Deprecated
 class TransitionManifold:
     def __init__(
-        self,
-        bandwidth_transitions: float,
-        bandwidth_diffusion_map: float = 1,
-        dimension: int = 10,
+            self,
+            bandwidth_transitions: float,
+            bandwidth_diffusion_map: float = 1,
+            dimension: int = 10,
     ):
         self.bandwidth_transitions = bandwidth_transitions
         self.bandwidth_diffusion_map = bandwidth_diffusion_map
@@ -131,188 +372,3 @@ class TransitionManifold:
             raise RuntimeError("No distance matrix available. Call the set_distance_matrix method first!")
 
         return np.mean(np.exp(-(self.distance_matrix ** 2) / epsilon))
-
-
-@njit(parallel=True)
-def _numba_dist_matrix_gaussian_kernel(
-    x_samples: np.ndarray, sigma: float
-) -> tuple[np.ndarray, np.ndarray]:
-    """
-    Parameters
-    ----------
-    x_samples : np.ndarray
-        Shape = (num_anchor_points, num_samples, dimension).
-    sigma : float
-        Bandwidth.
-
-    Returns
-    -------
-    tuple[np.ndarray, np.ndarray]
-        1) Distance matrix, shape = (num_anchor_points, num_anchor_points).
-        2) kernel matrix diagonal, shape = (num_anchor_points,)
-    """
-    num_anchor, num_samples, dimension = x_samples.shape
-
-    # compute symmetric kernel evaluations
-    kernel_diagonal = np.zeros(num_anchor)
-    for i in range(num_anchor):
-        kernel_diagonal[i] = _numba_gaussian_kernel_eval(
-            x_samples[i], x_samples[i], sigma
-        )
-    # compute asymmetric kernel evaluations and assemble distance matrix
-    distance_matrix = np.zeros((num_anchor, num_anchor))
-    for i in prange(num_anchor):
-        for j in range(i):
-            this_sum = _numba_gaussian_kernel_eval(x_samples[i], x_samples[j], sigma)
-            distance_matrix[i, j] = (
-                kernel_diagonal[i] + kernel_diagonal[j] - 2 * this_sum
-            )
-    distance_matrix /= num_samples**2
-    return distance_matrix + np.transpose(distance_matrix), kernel_diagonal
-
-
-@njit(parallel=False)
-def _numba_dist_matrix_gaussian_kernel_triangle_speedup(
-        x_samples: np.ndarray, sigma: float, triangle_speedup_tolerance=10e-4
-) -> tuple[np.ndarray, np.ndarray]:
-    """
-        Parameters
-        ----------
-        x_samples : np.ndarray
-            Shape = (num_anchor_points, num_samples, dimension).
-        sigma : float
-            Bandwidth.
-        triangle_speedup_tolerance : float
-            Tolerance for relative difference between lower and upper triangle inequality bound for applying the
-            speedup.
-            Defaults to 10e-4.
-
-        Returns
-        -------
-        tuple[np.ndarray, np.ndarray]
-            1) Distance matrix, shape = (num_anchor_points, num_anchor_points).
-            2) kernel matrix diagonal, shape = (num_anchor_points,)
-        """
-    logger.info(f"Computing distance matrix with triangle speedup. Rel error tolerance: {triangle_speedup_tolerance}")
-    num_anchor, num_samples, dimension = x_samples.shape
-
-    # compute symmetric kernel evaluations
-    kernel_diagonal = np.zeros(num_anchor)
-    for i in range(num_anchor):
-        kernel_diagonal[i] = _numba_gaussian_kernel_eval(
-            x_samples[i], x_samples[i], sigma
-        )
-
-    # compute asymmetric kernel evaluations and assemble distance matrix
-
-    n_speedup = 0
-
-    distance_matrix = np.zeros((num_anchor, num_anchor))
-    for i in range(num_anchor):
-        for j in range(i):
-
-            # Check if triangle inequality speedup can be applied
-            lower_max = -np.inf
-            upper_min = np.inf
-            for k in range(j):
-                lower_triang_bound = np.abs(distance_matrix[i, k] - distance_matrix[j, k])
-                upper_triang_bound = distance_matrix[i, k] + distance_matrix[j, k]
-
-                if lower_triang_bound > lower_max:
-                    lower_max = lower_triang_bound
-                if upper_triang_bound < upper_min:
-                    upper_min = upper_triang_bound
-
-            # If difference is within tolerance
-            if (upper_min - lower_max)/lower_max < triangle_speedup_tolerance:
-                distance_matrix[i, j] = 0.5 * (lower_max + upper_min)
-                n_speedup = n_speedup + 1
-                continue
-
-            this_sum = _numba_gaussian_kernel_eval(x_samples[i], x_samples[j], sigma)
-            distance_matrix[i, j] = (
-                kernel_diagonal[i] + kernel_diagonal[j] - 2 * this_sum
-            )
-
-    distance_matrix /= num_samples ** 2
-    return distance_matrix + np.transpose(distance_matrix), kernel_diagonal
-
-
-@njit(parallel=False)
-def _numba_gaussian_kernel_eval(x: np.ndarray, y: np.ndarray, sigma: float):
-    """
-    Parameters
-    ----------
-    x : np.ndarray
-        shape = (# x points, dimension)
-    y : np.ndarray
-        shape = (# y points, dimension)
-    sigma : float
-        bandwidth
-
-    Returns
-    -------
-    float
-        sum of kernel matrix
-    """
-    nx = x.shape[0]
-    ny = y.shape[0]
-
-    X = np.sum(x * x, axis=1).reshape((nx, 1)) * np.ones((1, ny))
-    Y = np.sum(y * y, axis=1) * np.ones((nx, 1))
-    out = X + Y - 2 * np.dot(x, y.T)
-    out /= -(sigma**2)
-    np.exp(out, out)
-    return np.sum(out)
-
-
-def calc_diffusion_maps(
-    distance_matrix: np.ndarray, num_components: int, sigma: float, alpha: float = 0.5
-) -> tuple[np.ndarray, np.ndarray]:
-    """
-    Solve diffusion map eigenproblem
-
-    Parameters
-    ----------
-    distance_matrix : np.ndarray
-        Symmetric array of shape (n_features, n_features).
-    num_components : int
-        Number of eigenvalues and eigenvectors to compute.
-    sigma : float
-        Diffusion map kernel bandwidth parameter.
-    alpha : float
-        Diffusion map normalization parameter.
-
-    Returns
-    -------
-    tuple[np.ndarray, np.ndarray]
-        Tuple of (eigenvalues, eigenvectors).
-    """
-
-    num_points = distance_matrix.shape[0]
-    kernel_matrix = np.exp(-(distance_matrix**2) / sigma**2)
-    row_sum = np.sum(kernel_matrix, axis=0)
-
-    # compensating for testpoint density
-    kernel_matrix = kernel_matrix / np.outer(row_sum**alpha, row_sum**alpha)
-
-    # row normalization
-    kernel_matrix = kernel_matrix / np.tile(sum(kernel_matrix, 0), (num_points, 1))
-
-    # weight matrix
-    weight_matrix = np.diag(sum(kernel_matrix, 0))
-
-    # solve the diffusion maps eigenproblem
-    return sla.eigs(kernel_matrix, num_components + 1, weight_matrix)
-
-
-def _central_differences(x: np.ndarray, y: np.ndarray) -> np.ndarray:
-    """
-    Compute dy/dx via central differences.
-    """
-    out = np.zeros(len(x))
-    for i in range(len(x)):
-        upper_idx = min(i + 1, len(x) - 1)
-        lower_idx = max(i - 1, 0)
-        out[i] = (y[upper_idx] - y[lower_idx]) / (x[upper_idx] - x[lower_idx])
-    return out
