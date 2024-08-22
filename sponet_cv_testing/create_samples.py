47adf2a56be257fdc0e6b9743d9c82681ae48d5d
MUrban <manuel.urban1999@gmail.com>
2024-08-22 10:49:38 +0200

    hotfix

--- sponet_cv_testing/create_samples.py
+++ sponet_cv_testing/create_samples.py
@@ -3,9 +3,9 @@ import logging
 import os
 import argparse
 from sponet_cv_testing.testpipeline import get_runfiles
-import sponet_cv_testing.resultmanagement as res
+from sponet_cv_testing import resultmanagement as res
 import numba
-import sponet_cv_testing.compute as comp
+from sponet_cv_testing import compute as comp
 from sponet_cv_testing.computation.run_method import (
     setup_dynamic,
     create_anchor_points,
